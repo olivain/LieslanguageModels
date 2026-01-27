0b5eefc05bcc741f2c65e1b23fb11c2852c454b8
olivain <olivain@users.noreply.github.com>
2026-01-27 22:47:04 +0100

    Update __init__.py

--- lieslm/__init__.py
+++ lieslm/__init__.py
@@ -2,6 +2,6 @@ from .vlm import VLMTrainer
 from .p2p import JetsonP2PNet
 from .img import JetsonCamera
 from .esp import create_hyphenated_epaper_image, send_png_to_esp
-from .led import blink_led
+from .led import blink_led, clean_led
 
-__all__ = ['VLMTrainer', 'JetsonP2PNet', 'create_hyphenated_epaper_image', 'send_png_to_esp', 'blink_led']
+__all__ = ['VLMTrainer', 'JetsonP2PNet', 'create_hyphenated_epaper_image', 'send_png_to_esp', 'blink_led', 'clean_led']
